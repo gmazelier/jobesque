45e0665fa8c53cdcd69dc7fd944770db575d2e9a
Gaylord Mazelier <gaylord@mazelier.fr>
2012-05-24 18:34:11 +0200

    Add some tests.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject jobesque "0.0.1-SNAPSHOT"
-  :description "Jobesque is a simple Clojure job scheduler, built on top of cron4j."
-  :url "https://github.com/gmazelier/jobesque"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [it.sauronsoftware/cron4j "2.2.5"]])
+	:description "Jobesque is a simple Clojure job scheduler, built on top of cron4j."
+	:url "https://github.com/gmazelier/jobesque"
+	:dependencies [[org.clojure/clojure "1.3.0"]
+				   [it.sauronsoftware/cron4j "2.2.5"]])
---
+++ test/jobesque/test/core.clj
@@ -0,0 +1,23 @@
+(ns jobesque.test.core
+	(:use [clojure.test])
+	(:require [jobesque.core :as job]))
+
+(deftest is-valid-every-minute
+	(is (nil? (job/is-valid-pattern "* * * * *"))))
+
+(deftest is-valid-every-five-minutes
+	(is (nil? (job/is-valid-pattern "*/5 * * * *"))))
+
+(deftest is-valid-every-minute-of-noon-on-monday
+	(is (nil? (job/is-valid-pattern "* 12 * * Mon"))))
+
+(deftest is-valid-every-working-day-at-noon
+	(is (nil? (job/is-valid-pattern "0 12 * * 1,2,3,4,5"))))
+(deftest is-not-valid-empty-string
+	(is (thrown? AssertionError (job/is-valid-pattern ""))))
+
+(deftest is-not-valid-another-string
+	(is (thrown? AssertionError (job/is-valid-pattern "this is not a valide pattern"))))
+
+(deftest is-not-valid-nil
+	(is (thrown? NullPointerException (job/is-valid-pattern nil))))
