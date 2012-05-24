6a21333903e10077338b479051b990c763630f3d
Gaylord Mazelier <gaylord@mazelier.fr>
2012-05-24 18:33:52 +0200

    Validate scheduling pattern before schedule.

--- src/jobesque/core.clj
+++ src/jobesque/core.clj
@@ -1,26 +1,32 @@
 (ns jobesque.core
-  (:import (it.sauronsoftware.cron4j Scheduler)))
+	(:import (it.sauronsoftware.cron4j Scheduler SchedulingPattern)))
 
 (def ^:dynamic *scheduler* (atom nil))
 
 (defn init
-  "Create a new scheduler instance."
-  ([]
-     (init (Scheduler.)))
-  ([scheduler]
-     (reset! *scheduler* scheduler)))
+	"Create a new scheduler instance."
+	([]
+		(init (Scheduler.)))
+	([scheduler]
+		(reset! *scheduler* scheduler)))
 
 (defn start
-  "Starts the scheduler."
-  []
-  (.start ^Scheduler @*scheduler*))
+	"Start the scheduler."
+	[]
+	(.start ^Scheduler @*scheduler*))
 
 (defn stop
-  "Stops the scheduler."
-  []
-  (.stop ^Scheduler @*scheduler*))
+	"Stop the scheduler."
+	[]
+	(.stop ^Scheduler @*scheduler*))
+
+(defn is-valid-pattern
+	"Return nil if the given pattern is valid, otherrise an AssertionError."
+	[pattern]
+	(assert (SchedulingPattern/validate pattern) (str "Pattern [" pattern "] not valid.")))
 
 (defn schedule
-  "Schedule a job given a scheduling pattern."
-  [pattern job]
-  (.schedule ^Scheduler @*scheduler* pattern job))
+	"Schedule a job given a scheduling pattern."
+	[pattern job]
+	(when (nil? (is-valid-pattern pattern))
+		(.schedule ^Scheduler @*scheduler* pattern job)))
