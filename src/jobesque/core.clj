75e8963a4283bb4f7a4585b6f336b82efcbf1e30
Gaylord Mazelier <gaylord@mazelier.fr>
2012-05-28 16:06:39 +0200

    Doc update.

--- src/jobesque/core.clj
+++ src/jobesque/core.clj
@@ -3,15 +3,15 @@
 
 (def ^:dynamic *scheduler* (atom nil))
 
-;; TODO Add "added" meta.
-
 (defn initialized?
 	"Return true if a scheduler instance exists, false otherwise."
+  {:added "0.0.1"}
 	[]
 	(instance? Scheduler @*scheduler*))
 
 (defn initialize
   "Create a new scheduler instance if not exists."
+  {:added "0.0.1"}
   ([]
    (initialize (Scheduler.)))
   ([scheduler]
@@ -20,30 +20,35 @@
 
 (defmacro when-initialized
   "Evaluates scheduler instance. If exists (logical true returned), evaluates body."
+  {:added "0.0.1"}
   [& body]
   `(when (initialized?)
     (do ~@body)))
 
 (defn start
 	"Starts the scheduler."
+  {:added "0.0.1"}
 	[]
 	(when-initialized
     (.start ^Scheduler @*scheduler*)))
 
 (defn stop
 	"Stops the scheduler."
+  {:added "0.0.1"}
 	[]
 	(when-initialized
     (.stop ^Scheduler @*scheduler*)))
 
 (defn valid-pattern? ;; TODO Change contract and return true or false ?
 	"Returns nil if the given pattern is valid, otherwise an AssertionError."
+  {:added "0.0.1"}
 	[pattern]
 	(assert (SchedulingPattern/validate pattern)
     (str "Pattern [" pattern "] not valid.")))
 
 (defn schedule
 	"Schedules a job given a scheduling pattern."
+  {:added "0.0.1"}
 	[pattern job]
   (when-initialized
     (when (nil? (valid-pattern? pattern))
