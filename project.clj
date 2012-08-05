a91f7032d9c89fe792ac06904c25c98805743373
Gaylord Mazelier <gaylord@mazelier.fr>
2012-08-05 22:21:56 +0200

    Start version 0.1.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jobesque "0.0.2"
+(defproject jobesque "0.1.0-SNAPSHOT"
                      :description "Jobesque is a simple Clojure job scheduler, built on top of cron4j."
                      :url "https://github.com/gmazelier/jobesque"
                      :dependencies [[org.clojure/clojure "1.3.0"]
